08b4a50db7faa58423ecf447c915625a4cc000bc
Nicolò Vescera <nicolo.vescera@gmail.com>
2022-07-08 17:30:13 +0000

    publish.sh undo commit
    Now the publish.sh undo the latest commit (adding dist/) and remove
    dist/ folder.

--- publish.sh
+++ publish.sh
@@ -2,4 +2,5 @@ git add dist && git commit -m "New build added"
 git subtree split --prefix dist -b gh-pages
 git push -f origin gh-pages:gh-pages
 git branch -D gh-pages
-# git subtree push --prefix dist origin gh-pages
+git reset --soft HEAD~1 # undo last commit
+rm -rf dist/            # remove dist folder

81453a6630bf93688c2dfb75b04714a81269ec1d
Nicolò Vescera <nicolo.vescera@gmail.com>
2022-07-08 12:40:28 +0000

    Fixed Publish.sh
    Fixed an error preventing push new builds

--- publish.sh
+++ publish.sh
@@ -1,2 +1,5 @@
 git add dist && git commit -m "New build added"
-git subtree push --prefix dist origin gh-pages
+git subtree split --prefix dist -b gh-pages
+git push -f origin gh-pages:gh-pages
+git branch -D gh-pages
+# git subtree push --prefix dist origin gh-pages

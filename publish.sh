dac5f0d45e6bed5b8507f2f151efa42546270838
Nicolò Vescera <nicolo.vescera@gmail.com>
2022-07-13 16:30:44 +0000

    New build added

--- publish.sh
+++
@@ -1,6 +0,0 @@
-git add dist && git commit -m "New build added"
-git subtree split --prefix dist -b gh-pages
-git push -f origin gh-pages:gh-pages
-git branch -D gh-pages
-git reset --soft HEAD~1 # undo last commit
-rm -rf dist/            # remove dist folder
--- src/main.js
+++
@@ -1,6 +0,0 @@
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
